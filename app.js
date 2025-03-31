b58023f83492fc517fffed9d3dd1a08b07931ff0
RynoCODE <rohit.roychoudhury13@gmail.com>
2025-03-31 09:42:36 +0530

    add compression middleware to improve response performance

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import router from "./routes/route.js";
 import morgan from 'morgan';
 import cors from 'cors'; 
+import compression from 'compression';
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors()); 
 app.use(express.json());
+app.use(compression());
 app.use(morgan('tiny'));
 
 app.use('/', router);
