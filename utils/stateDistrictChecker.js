38d8b36d110a1c924fa5d4a5b88e9ef29a0b846c
RynoCODE <rohit.roychoudhury13@gmail.com>
2025-03-31 16:23:24 +0530

    fix typo in import path for districts data in stateDistrictChecker

--- utils/stateDistrictChecker.js
+++ utils/stateDistrictChecker.js
@@ -1,4 +1,4 @@
-import districtsData from "../assests/districts.json" assert { type: "json" };
+import districtsData from "../assets/districts.json" assert { type: "json" };
 
 export const isValidStateAndDistrict = (state, district) => {
     const stateData = districtsData.states.find(s => s.state === state);
